1ff007b01100c7cb97298291e0818afbdd2f454c
Joey <joeyliao@outlook.com>
2019-07-31 20:52:30 -0700

    uploads the pics

--- day12/scripts/upload.js
+++ day12/scripts/upload.js
@@ -3,17 +3,22 @@ let s3Client = new AWS.S3({
   endpoint: 'http://127.0.0.1:9000',
   s3ForcePathStyle: true,
   signatureVersion: 'v4',
-  accessKeyId: 'YOUR ACCESS KEY',
-  secretAccessKey: 'YOUR SECRET KEY'
+  accessKeyId: '93ZPAX4LPYK1E8PEB3Z1',
+  secretAccessKey: 'BkvLejlOI+wEAlEO+Z51fOlzIjD8rkSex1L+slb+'
 });
+
+button = document.getElementById('submitBtn')
+file = document.getElementById('fileBtn')
+console.log(file.name)
+console.log(file)
 
 button.addEventListener('click', function (event) {
   s3Client.putObject({
     Bucket: 'images',
-    Key: file.name,
-    Body: file,
-    ContentLength: file.size,
-    ContentType: file.type
+    Key: file.files[0].name,
+    Body: file.files[0],
+    ContentLength: file.files[0].size,
+    ContentType: file.files[0].type
   }, function (err, data) {
     console.log(err, data);
   });
